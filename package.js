de6028816c982ade6221a5d35a6a331ffe973af2
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-05-20 14:21:51 -0600

    Updated references
    Updated to use Meteor 3.x
    Updated to use " instead of '
    Updated to latest versions of packages
    Updated to use the polygonwood:iron-router fork that supports Meteor 3.x instead

--- package.js
+++ package.js
@@ -4,38 +4,38 @@
 //   http://docs.meteor.com/#/full/packagejs
 //
 // and it is needed to define a Meteor package
-'use strict';
+"use strict";
 
 Package.describe({
-  name: 'useraccounts:iron-routing',
-  summary: 'UserAccounts package providing routes configuration capability via iron:router.',
-  version: '1.15.0',
-  git: 'https://github.com/meteor-compat/useraccounts-iron-routing.git'
+  name    : "useraccounts:iron-routing",
+  summary : "UserAccounts package providing routes configuration capability via iron:router.",
+  version : "1.16.0",
+  git     : "https://github.com/meteor-compat/useraccounts-iron-routing.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0.3');
+  api.versionsFrom("METEOR@3.2.2");
 
   api.use([
-    'check',
-    'iron:router@1.2.0',
-    'underscore',
-    'useraccounts:core@1.15.0',
-  ], ['client', 'server']);
+    "check@1.4.4",
+    "polygonwood:router@2.0.0",
+    "underscore@1.6.4",
+    "useraccounts:core@1.17.2",
+  ], ["client", "server"]);
 
   api.imply([
-    'useraccounts:core',
-    'iron:router',
-  ], ['client', 'server']);
+    "useraccounts:core@1.17.2",
+    "polygonwood:router@2.0.0"
+  ], ["client", "server"]);
 
   api.addFiles([
-    'lib/core.js',
-    'lib/server.js',
-  ], ['server']);
+    "lib/core.js",
+    "lib/server.js",
+  ], ["server"]);
 
   api.addFiles([
-    'lib/core.js',
-    'lib/client.js',
-    'lib/templates_helpers/at_input.js',
-  ], ['client']);
+    "lib/core.js",
+    "lib/client.js",
+    "lib/templates_helpers/at_input.js",
+  ], ["client"]);
 });
